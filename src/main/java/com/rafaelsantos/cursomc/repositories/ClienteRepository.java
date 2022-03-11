85c3117bd5961016f9c4f3733e53a25650a9e9ee
Rafael Santos <rafa.05125@gmail.com>
2022-03-11 06:32:15 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/rafaelsantos/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.rafaelsantos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelsantos.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
