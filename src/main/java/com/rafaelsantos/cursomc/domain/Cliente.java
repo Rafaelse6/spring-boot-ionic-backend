e8005a8df95de237d0ec0a88365c604f124ad706
Rafael Santos <rafa.05125@gmail.com>
2022-03-28 01:07:21 -0300

    Adicionando senha a Cliente

--- src/main/java/com/rafaelsantos/cursomc/domain/Cliente.java
+++ src/main/java/com/rafaelsantos/cursomc/domain/Cliente.java
@@ -36,6 +36,8 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonIgnore
+	private String senha;
 	
 	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -52,12 +54,13 @@ public class Cliente implements Serializable {
 		
 	}
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
+		this.senha= senha;
 		this.tipo = (tipo == null) ? null : tipo.getCod();
 	}
 
@@ -100,6 +103,14 @@ public class Cliente implements Serializable {
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
@@ -124,6 +135,8 @@ public class Cliente implements Serializable {
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
+	
+	
 
 	@Override
 	public int hashCode() {
@@ -141,6 +154,8 @@ public class Cliente implements Serializable {
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 
 }
