0c76a70cbbde89338a4a9ecaa3f67c5f8c9060fa
Rafael Santos <rafa.05125@gmail.com>
2022-03-30 15:50:01 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/rafaelsantos/cursomc/services/ClienteService.java
+++ src/main/java/com/rafaelsantos/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rafaelsantos.cursomc.domain.Cidade;
 import com.rafaelsantos.cursomc.domain.Cliente;
 import com.rafaelsantos.cursomc.domain.Endereco;
+import com.rafaelsantos.cursomc.domain.enums.Perfil;
 import com.rafaelsantos.cursomc.domain.enums.TipoCliente;
 import com.rafaelsantos.cursomc.dto.ClienteDTO;
 import com.rafaelsantos.cursomc.dto.ClienteNewDTO;
 import com.rafaelsantos.cursomc.repositories.ClienteRepository;
 import com.rafaelsantos.cursomc.repositories.EnderecoRepository;
+import com.rafaelsantos.cursomc.security.UserSS;
+import com.rafaelsantos.cursomc.services.exceptions.AuthorizationException;
 import com.rafaelsantos.cursomc.services.exceptions.DataIntegrityException;
 import com.rafaelsantos.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,13 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado"); 
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
---
+++ src/main/java/com/rafaelsantos/cursomc/services/UserService.java
@@ -0,0 +1,17 @@
+package com.rafaelsantos.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.rafaelsantos.cursomc.security.UserSS;
+
+public class UserService {
+	
+	public static UserSS  authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}
+		catch(Exception e ) {
+			return null;
+		}
+	}
+}
