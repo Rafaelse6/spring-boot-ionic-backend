ddbb129af44db10092911bff0437e47e72b03518
Rafael Santos <rafa.05125@gmail.com>
2022-04-08 15:35:56 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/rafaelsantos/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelsantos/cursomc/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.rafaelsantos.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelsantos.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,10 +19,8 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		
-		
-		
+		s3Service.uploadFile("E:\\tempImages\\fotos\\ana.jpg");
 		
 	}
 
